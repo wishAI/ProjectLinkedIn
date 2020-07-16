5fa7369253d386716bee713814eb77cbeb802704
wishAI <climax0@qq.com>
2020-07-15 19:28:25 -0700

    7:28

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ from pyquery import PyQuery as pq
 import requests as req
 import xlsxwriter
 
+
+
+# Data Definitions
 jobs = ['Analyst', 'Admin', 'Assistant', 'Junior', 'New Grads', 'Entry Level']
 job = 'Programmer'
-
-
-
 url_linkedin = 'https://ca.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=$key$&location=vancouver&trk=public_jobs_jobs-search-bar_search-submit&f_JT=F%2CT%2CC&f_TP=1%2C2&redirect=false&position=1&pageNum=0&start='
+url_indeed = 'https://ca.indeed.com/jobs?q=$key$&l=Vancouver%2C+BC&jt=$jt$&fromage=7&start='
+jts_indeed = ['fulltime', 'contract', 'temporary']
+
+# make a fake browser agent
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.90 Safari/537.36'}
+
+results = []
+
+
 
 # domain = 'https://ca.linkedin.com'
 # path = '/jobs-guest/jobs/api/seeMoreJobPostings/search'
@@ -18,72 +27,131 @@ url_linkedin = 'https://ca.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/s
 
 # template = domain + path + keywords + location + trk + other
 
-# make a fake browser agent
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.90 Safari/537.36'}
 
-results = []
 
+# Function Definitions
 def search_by_keyword(keyword):
-    start = 0
-    while start < 20000:
-        # build url and get response
-        url = url_linkedin.replace('$key$', keyword) + str(start)
-        print(url)
-        # url = template + str(start)
-        res = req.get(url, headers = headers)
-        if res.status_code != 200 or len(res.text) == 0:
-            print('No more results.')
-            break
-        doc = pq(res.text)
-        cards = doc('.result-card')
+    # print('')
+    # print('Fetch linkedin jobs!')
+    # print('')
+    # start = 0
+    # while start < 20000:
+    #     # build url and get response
+    #     url = url_linkedin.replace('$key$', keyword) + str(start)
+    #     res = req.get(url, headers = headers)
+    #     if res.status_code != 200 or len(res.text) == 0:
+    #         print('No more results.')
+    #         break
+    #     doc = pq(res.text)
+    #     cards = doc('.result-card')
 
-        # extract information from html
-        for card in cards.items():
-            title = card('.result-card__title').text()
-            subtitle = card('.result-card__subtitle').text()
-            location = card('.job-result-card__location').text()
-            time = card('time').attr('datetime')
-            href = card('.result-card__full-card-link').attr('href')
+    #     # extract information from html
+    #     for card in cards.items():
+    #         title = card('.result-card__title').text()
+    #         subtitle = card('.result-card__subtitle').text()
+    #         location = card('.job-result-card__location').text()
+    #         time = card('time').attr('datetime')
+    #         href = card('.result-card__full-card-link').attr('href')
             
-            # add card to results
-            row = (title, subtitle, location, time, href)
-            print(row)
-            results.append(row)
+    #         # add card to results
+    #         row = (title, subtitle, location, time, href, 'linkedin')
+    #         print(row)
+    #         results.append(row)
+    #     start += cards.size()
+
+    print('')
+    print('Fetch Indeed jobs!')
+    print('')
+    for jt in jts_indeed:
+        start = 0
+        job_num = 0
+        count = 0
+        url = url_indeed.replace('$key$', keyword)
+        url = url.replace('$jt$', jt)
         
-        start += cards.size()
+        while start < 20000:
+            url += str(start)
+            res = req.get(url, headers = headers)
+            doc = pq(res.text)
+            temp = doc('#searchCountPages').text()
+            job_num = int(temp.split()[3])
+            cards = doc('.jobsearch-SerpJobCard')
+            
+            for card in cards.items():
+                count += 1
+                title = card('.title a').text()
+                company = card('.company').text()
+                location = card('.location').text()
+                time = card('.date')
+                href = 'https://ca.indeed.com/' + card('.title a').attr('href')
+
+                row = (title, company, location, time, href, 'Indeed')
+                print(row)
+                results.append(row)
+            start += cards.size()
+
+            if count >= job_num - 1:
+                break
+
+            
 
 
+def linkedin_get_major(href):
+    res = req.get(href, headers = headers)
+    doc = pq(res.text)
+    criterias = doc('.job-criteria__item')
+    
+    majors = ''
+    
+    count = 0
+    for criteria in criterias.items():
+        if count == 3:
+            majors = criteria('.job-criteria__text').text()
+        count = count + 1
+
+    return majors
+
+
+
+# Procedures
+# Get all the records by keywords
 search_by_keyword('Assistant')
-
-
-# remove unsatified results
 print('')
 print('')
 print('Found ', len(results), ' records, start filtering.')
+
+
+# remove unsatified results
 def is_satisfied(result):
     loc = result[2]
-    if ('Delta' in loc) or ('Surrey' in loc) or ('delta' in loc) or ('surrey' in loc):
-        return False
-    return True
+    # !!! reversed the logic to in Vancouver, Richmond, and Burnaby
+    if ('Vancouver' in loc) or ('Richmond' in loc) or ('Burnaby' in loc):
+        return True
+    return False
 results = list(filter(is_satisfied, results))
 print(len(results), ' records after filtering.')
+
 
 # export results
 workbook = xlsxwriter.Workbook('results.xlsx')
 worksheet = workbook.add_worksheet()
 
 r = 0
-worksheet.write(r, 0, 'Title')
-worksheet.write(r, 1, 'Subtitle')
-worksheet.write(r, 2, 'Location')
-worksheet.write(r, 3, 'Time')
+worksheet.write(r, 0, 'Company')
+worksheet.write(r, 1, 'Job')
+worksheet.write(r, 2, 'Major')
+worksheet.write(r, 3, 'DDL')
 worksheet.write(r, 4, 'Link')
+
 r = 1
-for (title, subtitle, location, time, href) in results:
-    worksheet.write(r, 0, title)
-    worksheet.write(r, 1, subtitle)
-    worksheet.write(r, 2, location)
-    worksheet.write(r, 3, time)
+for (title, subtitle, location, time, href, site) in results:
+    majors = ''
+    # if site == 'linkedin':
+    #     majors= linkedin_get_major(href)
+    worksheet.write(r, 0, subtitle)
+    worksheet.write(r, 1, title)
+    worksheet.write(r, 2, majors)
+    worksheet.write(r, 3, 'None')
     worksheet.write(r, 4, href)
     r += 1
 workbook.close()
