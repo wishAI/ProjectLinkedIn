dda74c725e886ec527c610238463ab7a8f96e746
wishAI <climax0@qq.com>
2020-07-22 13:53:31 -0700

    1:53

--- main.py
+++ main.py
@@ -19,20 +19,6 @@ headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KH
 results = []
 
 
-
-# domain = 'https://ca.linkedin.com'
-# path = '/jobs-guest/jobs/api/seeMoreJobPostings/search'
-# keywords = '?keywords=' + job
-# location = '&location=%E6%B8%A9%E5%93%A5%E5%8D%8E%2C%20BC'
-# trk = '&trk=guest_homepage-basic_jobs-search-bar_search-submit'
-# other = '&redirect=false&pageNum=0&start='
-
-# template = domain + path + keywords + location + trk + other
-
-
-
-# Function Definitions
-
 def linkedin_search_keyword(keyword):
     print('')
     print('Fetch linkedin jobs!')
