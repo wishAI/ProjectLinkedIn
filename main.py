10e37de4d3d441bfaea5e0e6d9d3a6666699f228
wishAI <climax0@qq.com>
2020-07-22 13:52:02 -0700

    1:51

--- main.py
+++ main.py
@@ -32,69 +32,71 @@ results = []
 
 
 # Function Definitions
-def search_by_keyword(keyword):
-    # print('')
-    # print('Fetch linkedin jobs!')
-    # print('')
-    # start = 0
-    # while start < 20000:
-    #     # build url and get response
-    #     url = url_linkedin.replace('$key$', keyword) + str(start)
-    #     res = req.get(url, headers = headers)
-    #     if res.status_code != 200 or len(res.text) == 0:
-    #         print('No more results.')
-    #         break
-    #     doc = pq(res.text)
-    #     cards = doc('.result-card')
 
-    #     # extract information from html
-    #     for card in cards.items():
-    #         title = card('.result-card__title').text()
-    #         subtitle = card('.result-card__subtitle').text()
-    #         location = card('.job-result-card__location').text()
-    #         time = card('time').attr('datetime')
-    #         href = card('.result-card__full-card-link').attr('href')
+def linkedin_search_keyword(keyword):
+    print('')
+    print('Fetch linkedin jobs!')
+    print('')
+    start = 0
+    while start < 20000:
+        # build url and get response
+        url = url_linkedin.replace('$key$', keyword) + str(start)
+        res = req.get(url, headers = headers)
+        if res.status_code != 200 or len(res.text) == 0:
+            print('No more results.')
+            break
+        doc = pq(res.text)
+        cards = doc('.result-card')
+
+        # extract information from html
+        for card in cards.items():
+            title = card('.result-card__title').text()
+            subtitle = card('.result-card__subtitle').text()
+            location = card('.job-result-card__location').text()
+            time = card('time').attr('datetime')
+            href = card('.result-card__full-card-link').attr('href')
             
-    #         # add card to results
-    #         row = (title, subtitle, location, time, href, 'linkedin')
-    #         print(row)
-    #         results.append(row)
-    #     start += cards.size()
+            # add card to results
+            row = (title, subtitle, location, time, href, 'linkedin')
+            print(row)
+            results.append(row)
+        start += cards.size()
 
-    # print('')
-    # print('Fetch Indeed jobs!')
-    # print('')
-    # for jt in jts_indeed:
-    #     start = 0
-    #     job_num = 0
-    #     count = 0
-    #     url_temp = url_indeed.replace('$key$', keyword)
-    #     url_temp = url_temp.replace('$jt$', jt)
+def indeed_search_keyword(keyword):
+    print('')
+    print('Fetch Indeed jobs!')
+    print('')
+    for jt in jts_indeed:
+        start = 0
+        job_num = 0
+        count = 0
+        url_temp = url_indeed.replace('$key$', keyword)
+        url_temp = url_temp.replace('$jt$', jt)
         
-    #     while start < 20000:
-    #         url = url_temp + str(start)
-    #         res = req.get(url, headers = headers)
-    #         doc = pq(res.text)
-    #         temp = doc('#searchCountPages').text()
-    #         job_num = int(temp.split()[3])
-    #         cards = doc('.jobsearch-SerpJobCard')
+        while start < 20000:
+            url = url_temp + str(start)
+            res = req.get(url, headers = headers)
+            doc = pq(res.text)
+            temp = doc('#searchCountPages').text()
+            job_num = int(temp.split()[3])
+            cards = doc('.jobsearch-SerpJobCard')
             
-    #         for card in cards.items():
-    #             count += 1
-    #             title = card('.title a').text()
-    #             company = card('.company').text()
-    #             location = card('.location').text()
-    #             time = card('.date')
-    #             href = 'https://ca.indeed.com/' + card('.title a').attr('href')
+            for card in cards.items():
+                count += 1
+                title = card('.title a').text()
+                company = card('.company').text()
+                location = card('.location').text()
+                time = card('.date')
+                href = 'https://ca.indeed.com/' + card('.title a').attr('href')
 
-    #             row = (title, company, location, time, href, 'Indeed')
-    #             print(row)
-    #             results.append(row)
-    #         start += cards.size()
-    #         if count >= job_num - 1:
-    #             break
+                row = (title, company, location, time, href, 'Indeed')
+                print(row)
+                results.append(row)
+            start += cards.size()
+            if count >= job_num - 1:
+                break
 
-    
+def glass_search_keyword(keyword):
     print('')
     print('Fetch Glassdoor jobs!')
     print('')
@@ -123,17 +125,13 @@ def search_by_keyword(keyword):
                 results.append(row)
             if current >= page_num:
                 break
-            current += 1
+            current += 1      
 
 
-# remove unsatified results
-def is_satisfied(result):
-    loc = result[2]
-    # !!! reversed the logic to in Vancouver, Richmond, and Burnaby
-    if ('Vancouver' in loc) or ('Richmond' in loc) or ('Burnaby' in loc):
-        return True
-    return False            
-
+def search_by_keyword(keyword):
+    linkedin_search_keyword(keyword)
+    indeed_search_keyword(keyword)
+    glass_search_keyword(keyword)
 
 def linkedin_get_major(href):
     res = req.get(href, headers = headers)
@@ -150,17 +148,73 @@ def linkedin_get_major(href):
 
     return majors
 
+def linkedin_get_size(href):
+    return False
+
+def indeed_get_major(href):
+    return False
+
+def indeed_get_size(href):
+    return False
+
+def glass_get_major(href):
+    return False
+
+def glass_get_size(href):
+    return False
+
+# remove unsatified results
+def is_satisfied(result):
+    loc = result[2]
+    # !!! reversed the logic to in Vancouver, Richmond, and Burnaby
+    if ('Vancouver' in loc) or ('Richmond' in loc) or ('Burnaby' in loc):
+        return True
+    return False
+
+def is_duplicated(r1, r2):
+    link1 = r1[4]
+    link2 = r2[4]
+    if link1 == link2:
+        return True
+    job1 = r1[0]
+    job2 = r2[0]
+    company1 = r1[1]
+    company2 = r2[1]
+    loc1 = r1[2]
+    loc2 = r2[2]
+    if job1 == job2 and company1 == company2:
+        if ('Vancouver' in loc1) and ('Vancouver' in loc2):
+            return True
+        if ('Richmond' in loc1) and ('Richmond' in loc2):
+            return True
+        if ('Burnaby' in loc1) and ('Burnaby' in loc2):
+            return True
+    return False
+
+def remove_duplicated(l, f):
+    newL = []
+    for e in l:
+        for newE in newL:
+            if f(e, newE):
+                continue
+        newL.append(e)
+    return newL
+
+      
 
 
 # Procedures
 # Get all the records by keywords
-search_by_keyword('Assistant')
+for job in jobs:
+    search_by_keyword(job)
 print('')
 print('')
 print('Found ', len(results), ' records, start filtering.')
 
 results = list(filter(is_satisfied, results))
 print(len(results), ' records after filtering.')
+results = remove_duplicated(results, is_duplicated)
+print(len(results), ' records after removing duplicated items.')
 
 
 # export results
